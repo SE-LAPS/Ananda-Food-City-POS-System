5f65baf61eae6a2b4f1b01b8705ae92f644d4225
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2023-10-21 23:59:13 +0530

    Add partials files

---
+++ Restro/admin/partials/_footer.php
@@ -0,0 +1,18 @@
+<footer class="py-5">
+  <div class="container">
+    <div class="row align-items-center justify-content-xl-between">
+      <div class="col-xl-6">
+        <div class="copyright text-center text-xl-left text-white"> <!-- Change text color to white -->
+          &copy; 2023 - <?php echo date('Y'); ?> - Developed By Group-10 - 21.1-NSBM
+        </div>
+      </div>
+      <div class="col-xl-6">
+        <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+          <li class="nav-item">
+            <a href="../../index.php" class="nav-link text-white">BACK</a> <!-- Change text color to white -->
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
